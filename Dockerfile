f81d40bc85dc8d3531d98663212870ecfae2a91e
Piotr Bechcicki <piotr.bechcicki@codilime.com>
2019-06-11 03:17:49 +0200

    Update ace-collab. Provide host and allowed origins as arguments.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM node:8
 
 ARG workdir=/usr/src/ace-collab-server
-
 ARG port=3333
+ARG HOST="0.0.0.0"
+ARG allowedOrigins=[]
 
 WORKDIR ${workdir}
 
+ENV ALLOWED_ORIGINS=${allowedOrigins}
+ENV HOST=${host}
 ENV PORT=${port}
 
 COPY package*.json ./
@@ -15,4 +18,4 @@ RUN npm install
 COPY . .
 
 EXPOSE ${port}
-CMD [ "npm", "start" ]
+CMD ["npm", "start" ]
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const startServer = require('ace-collab/lib/server/index.js')
 
+console.log(process.env.ALLOWED_ORIGINS)
+
+const allowedOrigins = !!process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS : []
+const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3333
 
-startServer(port)
+const options = {
+    allowedOrigins,
+    host,
+    port,
+}
+
+startServer(options)
