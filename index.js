60bcabe2e328b64ede4065a0320ff70618c809cd
Piotr Bechcicki <piotr.bechcicki@codilime.com>
2019-09-21 16:22:36 +0200

    Update ace-collab to 0.14.0.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log(process.env.ALLOWED_ORIGINS)
 const allowedOrigins = !!process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS : []
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3333
+const readOnly = process.env.READ_ONLY || false
 
 const options = {
     allowedOrigins,
